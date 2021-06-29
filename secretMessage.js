8d0da313303d40bef7d86e926a29e704fee79043
Nargis <nargis.salim@btinternet.com>
2021-06-29 14:21:12 +0100

    changed element by accessing index and replacing it

--- secretMessage.js
+++ secretMessage.js
@@ -11,23 +11,30 @@ console.log('SECRET MESSAGE');
 console.log('Length of original message: ' + secretMessage.length);
 // Output: Length of original message: 24
 
-// return removed last string of the array secretMessage
-// console.log('Return removed item: ' + secretMessage.pop());
-// Output: Return removed item: JavaScript
 
-// remove the last string of the array secretMessage
+// return removed last element of the array secretMessage
+// console.log('Return removed element: ' + secretMessage.pop());
+// Output: Return removed element: JavaScript
+
+
+// remove the last element of the array secretMessage
 secretMessage.pop();
 // console.log(secretMessage);
 
+
 // check length is 1 less than the original length
-console.log('Length of message (after last string removed): ' + secretMessage.length);
-// Output: Length of message (after last string removed): 23
+console.log('Length of message (after last element removed): ' + secretMessage.length);
+// Output: Length of message (after last element removed): 23
 
 // add the words 'to' and 'Program' as separate strings to the end of the secretMessage array
 secretMessage.push('to', 'Program');
 // Output shows 'to' and 'Program' added to the end of the array
+console.log("Add to end of array: 'to' and 'Program'")
 
-// change the word easily to the word right by accessing the index and replacing it
+
+// change the element 'easily' to the element 'right' by accessing the index and replacing it
+secretMessage[7] = 'right';
+console.log("Replace element 'easily' with: " + "'" + secretMessage[7] + "'");
 
 
 // remove the first string of the array
