e0683132aca6796323eb37c6fd2e94f48f0cc54b
Nargis <nargis.salim@btinternet.com>
2021-06-29 15:14:38 +0100

    remove or add elements at beginning of array

--- secretMessage.js
+++ secretMessage.js
@@ -37,15 +37,22 @@ secretMessage[7] = 'right';
 console.log("Replace element 'easily' with: " + "'" + secretMessage[7] + "'");
 
 
-// remove the first string of the array
+// return removed first element of the array secretMessage
+// console.log("Return removed first element: " + "'" + secretMessage.shift() + "'");
+// Output: Return removed first element: 'Learning'
+console.log("Original first element: " + "'" + secretMessage[0] + "'")
+// Output: Original first element: 'Learning'
 
+// remove the first string of the array
+secretMessage.shift();
+console.log("New first element (after removal): " + "'" + secretMessage[0] + "'");
+// Output: New first element (after removal): 'is'
 
 // add the string 'Programming' to the beginning of the array
-
+secretMessage.unshift('Programming');
+console.log("New first element (added): " + "'" + secretMessage[0] + "'");
 
 // remove the strings 'get', 'right', 'the', 'first', 'time', and replace them with the single string 'know'
 
 
 // use console.log() and .join() to print the secret message as a sentence
-
-
