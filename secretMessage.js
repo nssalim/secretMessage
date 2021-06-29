1bf2eed97b413ae1b3057ea8a23c4b2b5802f684
Nargis <nargis.salim@btinternet.com>
2021-06-29 11:58:58 +0100

    first commit

---
+++ secretMessage.js
@@ -0,0 +1,39 @@
+// Secret Message
+// Use array methods to transform an array of strings into a secret message!
+
+let secretMessage = ['Learning', 'is', 'not', 'about', 'what', 'you', 'get', 'easily', 'the', 'first', 'time,', 'it', 'is', 'about', 'what', 'you', 'can', 'figure', 'out.', '-2015,', 'Chris', 'Pine,', 'Learn', 'JavaScript'];
+
+// Heading
+console.log('Secret Message')
+
+// remove the last string of the array secretMessage
+
+
+// check length is 1 less than the original length
+
+
+// add the words 'to' and 'Program' as separate strings to the end of the secretMessage array
+
+
+// change the word easily to the word right by accessing the index and replacing it
+
+
+// remove the first string of the array
+
+
+// add the string 'Programming' to the beginning of the array
+
+
+// remove the strings 'get', 'right', 'the', 'first', 'time', and replace them with the single string 'know'
+
+
+// use console.log() and .join() to print the secret message as a sentence
+
+
+
+
+
+
+
+
+
